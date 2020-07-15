efd94e948f724f3925232ee7d2e6c97a824a400d
Patrick Tkacik <c4tachan@gmail.com>
2020-07-15 14:26:29 -0400

    image!

--- raceanalyst/raceanalyst/DataModel.cs
+++ raceanalyst/raceanalyst/DataModel.cs
@@ -12,6 +12,13 @@ namespace raceanalyst
 {
     class DataModel
     {
+        private struct character
+        {
+            public int ord;
+            public int x;
+            public int y;
+        }
+
         private DataModel() { }
 
         private static DataModel me = new DataModel();
@@ -19,7 +26,7 @@ namespace raceanalyst
         private List<Mat> chars = new List<Mat>();
         private List<int> labels = new List<int>();
 
-        private OpenCvSharp.ML.SVM svm;
+        private OpenCvSharp.ML.SVM model;
 
         private const string modelFile = "svm_model.dat";
 
@@ -47,6 +54,34 @@ namespace raceanalyst
             }
         }
 
+        private Mat Deskew(Mat img)
+        {
+            Moments m = new Moments(img);
+            Mat deskewed_img = new Mat();
+
+            if(Math.Abs(m.Mu02) < 0.01)
+            {
+                deskewed_img = img.Clone();
+                return deskewed_img;
+            }
+
+            float skew = (float)(m.Mu11 / m.Mu02);
+            float[,] M_vals = new float[,] { { 1, skew, (float)( 0.5 * 60 * skew) }, { 0, 1, 0 } };
+            Mat M = new Mat(new OpenCvSharp.Size(3, 2), MatType.CV_32F);
+
+            for (int i = 0; i < M.Rows; i++)
+            {
+                for (int j = 0; j < M.Cols; j++)
+                {
+                    M.At<float>(i, j) = M_vals[i,j];
+                }
+            }
+
+            Cv2.WarpAffine(img, deskewed_img, M, new OpenCvSharp.Size(60, 30), InterpolationFlags.WarpInverseMap | InterpolationFlags.Linear);
+
+            return deskewed_img;
+        }
+
         private List<double> bincount(Mat x, Mat weights, int min_length)
         {
             double max_x_val = 0;
@@ -67,22 +102,22 @@ namespace raceanalyst
             return bins.ToList();
         }
 
-        private Mat Preprocess_hog()
+        private Mat Preprocess_hog(List<Mat> input)
         {
             int bin_n = 16;
             int half_x = 30;
             int half_y = 15;
             double eps = 1e-7;
 
-            var hog = new Mat(new OpenCvSharp.Size(4 * bin_n, chars.Count), MatType.CV_32F);
+            var hog = new Mat(new OpenCvSharp.Size(4 * bin_n, input.Count), MatType.CV_32F);
 
-            for(int img_index = 0; img_index < chars.Count; img_index++)
+            for(int img_index = 0; img_index < input.Count; img_index++)
             {
                 var gx = new Mat();
-                Cv2.Sobel(chars[img_index], gx, MatType.CV_32F, 1, 0);
+                Cv2.Sobel(input[img_index], gx, MatType.CV_32F, 1, 0);
 
                 var gy = new Mat();
-                Cv2.Sobel(chars[img_index], gy, MatType.CV_32F, 0, 1);
+                Cv2.Sobel(input[img_index], gy, MatType.CV_32F, 0, 1);
 
                 var mag = new Mat();
                 var ang = new Mat();
@@ -173,24 +208,80 @@ namespace raceanalyst
                 }
             }
 
-            Mat samples = Preprocess_hog();
+            Mat samples = Preprocess_hog(chars);
 
-            svm = OpenCvSharp.ML.SVM.Create();
+            model = OpenCvSharp.ML.SVM.Create();
 
-            svm.Gamma = 5.383;
-            svm.C = 2.67;
-            svm.KernelType = OpenCvSharp.ML.SVM.KernelTypes.Rbf;
-            svm.Type = OpenCvSharp.ML.SVM.Types.CSvc;
+            model.Gamma = 5.383;
+            model.C = 2.67;
+            model.KernelType = OpenCvSharp.ML.SVM.KernelTypes.Rbf;
+            model.Type = OpenCvSharp.ML.SVM.Types.CSvc;
 
-            svm.Train(samples, OpenCvSharp.ML.SampleTypes.RowSample, OpenCvSharp.InputArray.Create(labels));
+            model.Train(samples, OpenCvSharp.ML.SampleTypes.RowSample, OpenCvSharp.InputArray.Create(labels));
 
-            svm.Save(modelFile);
+            model.Save(modelFile);
         }
 
         internal void Analyze(string analyzeImage)
         {
-            svm = OpenCvSharp.ML.SVM.Load(modelFile);
+            model = OpenCvSharp.ML.SVM.Load(modelFile);
 
+            Console.WriteLine($"Loading {analyzeImage} . . .");
+            var img = new Mat(analyzeImage, ImreadModes.Grayscale);
+
+            Mat threshbin = new Mat();
+            Cv2.AdaptiveThreshold(img, threshbin, 1.0, AdaptiveThresholdTypes.MeanC, ThresholdTypes.BinaryInv, 31, 10);
+
+            Mat bin = new Mat();
+            Cv2.MedianBlur(threshbin, bin, 3);
+
+            HierarchyIndex[] heirs;
+            OpenCvSharp.Point[][] contours;
+            bin.FindContours(out contours, out heirs, RetrievalModes.CComp, ContourApproximationModes.ApproxSimple);
+                
+
+            List<character> chrs = new List<character>();
+            foreach (var contour in contours)
+            {
+                // Draw a rect around the contour!
+                var boundingRect = Cv2.BoundingRect(contour);
+                if (boundingRect.Height > 1 && boundingRect.Width > 1)
+                {
+                    // Let's analyze each contour!
+                    var bin_norms = new List<Mat>();
+                    bin_norms.Add(Deskew(new Mat(img, boundingRect)));
+
+
+                    var sample = Preprocess_hog(bin_norms);
+
+                    var ord1 = (int)model.Predict(sample);
+
+                    chrs.Add(new character()
+                    {
+                        ord = ord1,
+                        x = boundingRect.Left,
+                        y = boundingRect.Top
+                    });
+
+                    if ((ord1 >= Convert.ToInt32(' '))
+                        && (ord1 < Convert.ToInt32('~')))
+                    {
+                        Cv2.Rectangle(img, boundingRect, 255);
+                        Cv2.PutText(img, $"{Convert.ToChar(ord1)}", boundingRect.TopLeft, HersheyFonts.HersheyPlain, 3.0, 255);
+                    }
+                }
+            }
+
+            var sorted_chars = from chr in chrs
+                               orderby chr.y ascending
+                               orderby chr.x ascending
+                               select chr;
+
+
+            using (new Window(img))
+            {
+                Cv2.WaitKey();
+            }
 
         }
     }
